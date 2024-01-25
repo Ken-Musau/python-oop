d0bd89cc100704971aa0ff43410e8c6f9b739367
Kennedy Musau <kennedy.musauu@gmail.com>
2024-01-25 22:35:24 +0300

    add book class

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_get_page_count(self, page_count):
+        if type(page_count) is int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    page_count = property(get_page_count, set_get_page_count)
